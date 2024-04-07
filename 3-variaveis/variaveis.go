94c7200fafd2ea15a969a2d6d0b3b23e4eb751df
douglas.acioli@gmail.com <douglas.acioli@gmail.com>
2024-04-07 09:05:15 -0300

    variaveis e declaracao curta de variaveis

--- 3-variaveis/variaveis.go
+++ 3-variaveis/variaveis.go
@@ -4,9 +4,27 @@ import (
 	"fmt"
 )
 
-var c, python, java bool
+var ruby, php, golang bool //a função var declara uma lista de variaveis
+// func main() {
+// 	var inteiro int
+// 	fmt.Println(inteiro, ruby, php, golang)
 
+// }
+
+// -------------------------------------------------------------------------
+var i, j int = 1, 2 //inicializando variaveis
+// func main() {
+// 	var c, python, java = true, false, "no!"
+// 	fmt.Println(i, j, c, python, java)
+// }
+
+// declaração curta de variaveis
+// :=
+// só é possivel usar dentro de uma função
 func main() {
-	var i int
-	fmt.Println(i, c, python, java)
+	var i, j int = 1, 2
+	k := 3
+	c, python, java := true, false, "no!"
+
+	fmt.Println(i, j, k, c, python, java)
 }
