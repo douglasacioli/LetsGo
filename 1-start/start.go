aa2fdbdacb9abba86652494c986f77528da3fa0f
douglas.acioli@gmail.com <douglas.acioli@gmail.com>
2024-04-07 08:35:00 -0300

    funcoes

---
+++ 1-start/start.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("Meu numero favorito é ", rand.Intn(10))
+	fmt.Println(math.Pi)
+}
+
+//dentro do import fazemos a declaração dos pacotes que queremos usar
+//fmt funções para formatação de string
+// math/rand funções matematicas
